729a646a0a6bc3c172e8fa8cd1e1da731e35f54f
kukunechka007 <156849628+kukunechka007@users.noreply.github.com>
2025-01-18 16:57:17 +0100

    Add files via upload

---
+++ src/App.tsx
@@ -0,0 +1,10 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom"
+const App = () => {
+  return (
+    <BrowserRouter>
+      WeatherApp
+    </BrowserRouter>
+  )
+}
+
+export default App
